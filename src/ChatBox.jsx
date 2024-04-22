27264f9b711c3e51cb7d64811a7c4f70b989d33f
Bram Schabbink <73956679+brambrulant@users.noreply.github.com>
2024-04-22 16:08:46 +0200

    remove text so i can keep my job

--- src/ChatBox.jsx
+++ src/ChatBox.jsx
@@ -12,7 +12,7 @@ export default function ChatBox({ open }) {
       >
         <div className="w-full h-14 bg-[#25D366] rounded-t-xl flex flex-row items-center">
           <div className="bg-white rounded-full h-8 w-8 ml-4 text-center">
-            <div className="mt-1">DB</div>
+            <div className="mt-1">MB</div>
           </div>
         </div>
         <motion.div
@@ -29,15 +29,23 @@ export default function ChatBox({ open }) {
           transition={{ duration: 0.4, delay: 2.5 }}
           className="bg-[#E0ECBE] mx-4 my-2 rounded-md p-2"
         >
-          Hoe staat het met de productiviteit?
+          Heb je een update over die nieuwe feature?
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.4, delay: 4.5 }}
+          transition={{ duration: 0.4, delay: 8.5 }}
           className="bg-[#E0ECBE] mx-4 my-2 rounded-md p-2"
         >
-          en we moeten snel even samen kijken naar de kwaliteit!!!
+          ?
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.4, delay: 9.5 }}
+          className="bg-[#E0ECBE] mx-4 my-2 rounded-md p-2"
+        >
+          ????
         </motion.div>
       </motion.div>
     );
